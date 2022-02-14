21a68eaf314875f4d59aff290e439676310f0254
Camila Cristina <criscamila09@gmail.com>
2022-02-14 11:51:50 -0300

    refatorando o caso de uso de categoria

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,51 +1,44 @@
-/* eslint-disable no-shadow */
-/* eslint-disable import/prefer-default-export */
-import { Category } from "../../model/Category";
+import { getRepository, Repository } from "typeorm";
+
+import { Category } from "../../entities/Category";
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
 } from "../ICategoriesRepository";
+
+
 
 // DTO -> Data Transfer Object: criar um objeto que vai ser responsavel pela tranferencia de dados entre uma classe e outra
 
 // singleton -> padrão de projeto, tem como definição criar a penas uma instacia de uma classe
 
 class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[];
+  
+  private repository: Repository<Category>;
 
-  private static INSTANCE: CategoriesRepository;
-
-  private constructor() {
-    this.categories = [];
+  constructor() {
+    this.repository = getRepository(Category);
   }
 
-  public static getInstance(): CategoriesRepository {
-    if (!CategoriesRepository.INSTANCE) {
-      CategoriesRepository.INSTANCE = new CategoriesRepository();
-    }
-    return CategoriesRepository.INSTANCE;
-  }
 
-  create({ description, name }: ICreateCategoryDTO): void {
-    const category = new Category();
-
-    Object.assign(category, {
-      // função do JS . consegue passar um objeto pra ele, consegue passar o satributos que eu quero para esse o objeto(category)
-      name,
+  async create({ description, name }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
       description,
-      created_at: new Date(),
+      name,
     });
 
-    this.categories.push(category);
+    await this.repository.save(category)
   }
 
-  list(): Category[] {
+  async list(): Promise<Category[]> {
     // pare ter o retorno da nossa lista de categorias
-    return this.categories;
+    const categories = await this.repository.find();
+    return categories;
   }
 
-  findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
+  async findByName(name: string): Promise<Category> {
+    //Select * from categories where name = "name"
+    const category = await this.repository.findOne({name});
 
     return category;
   }

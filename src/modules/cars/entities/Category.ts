d8c15eb5ce4354afc51cb65d19517188a53f99f6
Camila Cristina <criscamila09@gmail.com>
2022-02-14 10:50:17 -0300

    alterando o repositorio de categoria

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,13 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 import { v4 as uuidV4 } from "uuid";
 
+
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
-  // eslint-disable-next-line camelcase
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {

24399e1fb017123f5a6e1d5283bd5d837799385b
Camila Cristina <criscamila09@gmail.com>
2022-02-15 15:01:54 -0300

    criptografar senha

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,8 +1,8 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
-import { UsersRepository } from "../../repositories/implementations/UsersRepository";
 
 @injectable()
 class CreateUserUseCase {
@@ -17,10 +17,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }

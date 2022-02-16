5ca7e9927657c6de7da16f500f9d2e2d22db1f3b
Camila Cristina <criscamila09@gmail.com>
2022-02-15 21:02:02 -0300

    autenticação nas rotas

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
 interface IPayload {
   sub: string;
@@ -22,11 +23,17 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub } = verify(
+    const { sub: user_id } = verify(
       token,
       "cfe275a5908b5650488e0b0342c2d6cc",
     ) as IPayload;
-    console.log(sub);
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not existis!");
+    }
 
     next();
   } catch {

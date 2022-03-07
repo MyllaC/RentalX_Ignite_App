a17efd8beb26096f529b59703ea660533fb0c9fe
Camila Cristina <criscamila09@gmail.com>
2022-03-07 13:40:55 -0300

    criando seed de usuário

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,21 +3,18 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import cors from "cors";
-
-import "@shared/infra/typeorm";
-
 import "@shared/container";
-
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
 
-app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);

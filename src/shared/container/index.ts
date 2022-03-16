b3ccbbdafdae28d5ac1714eb352b08a513924c25
Camila Cristina <criscamila09@gmail.com>
2022-03-16 12:18:17 -0300

    repositório de refresh token

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,21 +2,21 @@ import { container } from "tsyringe";
 
 import "@shared/container/providers";
 
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
-import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
-
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
-import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
-import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
-import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
-import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 
-// Passar a ICategoriesRepository dentro do singleton
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository,
@@ -42,4 +42,9 @@ container.registerSingleton<ICarsImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   "RentalsRepository",
   RentalsRepository,
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  "UsersTokensRepository",
+  UsersTokensRepository,
 );

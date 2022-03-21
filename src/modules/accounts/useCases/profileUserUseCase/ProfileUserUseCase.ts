78f5a766665ecffcb134f94344c9f2e7a83c1e4d
Camila Cristina <criscamila09@gmail.com>
2022-03-21 19:53:04 -0300

    mapper

--- src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
@@ -9,8 +9,9 @@ import { UserMap } from "@modules/accounts/mapper/UserMap";
 class ProfileUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
-  ) {}
+    private usersRepository: IUsersRepository,
+  ) { }
+
   async execute(id: string): Promise<IUserResponseDTO> {
     const user = await this.usersRepository.findById(id);
 
